ed40ac07d45a1e4e8225414477d658cddc6b4cce
Your mato <you@example.com>
2020-10-17 00:49:09 +0800

    Initial Commit

---
+++ exchange rate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exchange rate
+//
+//  Created by kittawat phuangsombat on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
