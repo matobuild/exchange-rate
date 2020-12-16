d69d1b570159db83f8267a46bc253d9e16e4cc2b
Your mato <you@example.com>
2020-12-16 22:24:59 +0800

    put currencies into the right alphabet

--- exchange rate/Controller/currencyController.swift
+++ exchange rate/Controller/currencyController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class currencyController: UIViewController, UITableViewDelegate,UITableViewDataSource{
- 
+    
     let cellReuseIdentifier = K.neededCell
     
     @IBOutlet weak var tableView: UITableView!
@@ -19,30 +19,36 @@ class currencyController: UIViewController, UITableViewDelegate,UITableViewDataS
         tableView.dataSource = self
         
         dictToArray()
-       
+        putCurrencyIntoDictionary()
+        
     }
-
+    
     //number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayCounted()
+        let currencyKey = findCurrencyTitle()[section]
+        if let currencyValues = currencyDictionary[currencyKey]{
+            return currencyValues.count
+        }
+        return 0
+        
     }
     
     //create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:MyCustomCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! MyCustomCell
         
-        let sortedCurrency = sortedCurrencies()
-
-        cell.myCurrencyCellLabel.text = sortedCurrency[indexPath.row].unit
-        
-        cell.currencyFullName.text = sortedCurrency[indexPath.row].unitName
- 
+        let currencyKey = findCurrencyTitle()[indexPath.section]
+        if let currencyValue = currencyDictionary[currencyKey]{
+            cell.myCurrencyCellLabel.text = currencyValue[indexPath.row].unit
+            cell.currencyFullName.text = currencyValue[indexPath.row].unitName
+        }
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-
+        
         let tapCurrency = getTap(index: indexPath)
         
         //clear all array
@@ -52,51 +58,45 @@ class currencyController: UIViewController, UITableViewDelegate,UITableViewDataS
             dismiss(animated: true) {
                 vc.setCurrency(tapCurrency)
             }
-            
         }
         
-    
     }
- 
-
+    
     func getTap(index: IndexPath) -> Objects {
         let currencyInitial = sortedCurrencies()[index.row].unit!
         let currencyFullName = sortedCurrencies()[index.row].unitName!
         let object = Objects(unit: currencyInitial, unitName: currencyFullName    )
         return object
     }
- 
+    
 }
 //MARK: - add title
-
-
-
 extension currencyController{
-
+    
     // adding section
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return findCurrencyTitle().count
-        }
-
-        func tableView(_ tableView: UITableView,
-                       titleForHeaderInSection section: Int) -> String? {
-            let sortedCurrency = findCurrencyTitle()
-            let currencySectionTitles = sortedCurrency[section]
-            return currencySectionTitles
-        }
-//
-//    Adding the Section Index
-//
-        func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-            findCurrencyTitle()
-        }
-
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   titleForHeaderInSection section: Int) -> String? {
+        let sortedCurrency = findCurrencyTitle()
+        let currencySectionTitles = sortedCurrency[section]
+        return currencySectionTitles
+    }
+    
+    //    Adding the Section Index
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        findCurrencyTitle()
+    }
+    
     func tableView(_ tableView: UITableView,
                    sectionForSectionIndexTitle title: String,
                    at index: Int) -> Int {
         index
     }
-
+    
 }
 
--- exchange rate/View/for list currency/Objects.swift
+++ exchange rate/View/for list currency/Objects.swift
@@ -15,7 +15,8 @@ struct Objects {
 var currency = Currency()
 var objectArray = [Objects]()
 
-//var currencySectionTitles = [String]()
+var currencyDictionary = [String: [Objects]]()
+
 
 func dictToArray() {
     for (key,value) in currency.currenciesUnit{
@@ -31,30 +32,34 @@ func sortedCurrencies()-> [Objects]  {
     objectArray.sorted(by: { $0.unit < $1.unit })
 }
 
-func findCurrencyTitle()-> [String] {
+func findCurrencyTitle() -> [String] {
     var currencyTitle = [String]()
     
     for currency in sortedCurrencies(){
         let currencyKey = String(currency.unit.prefix(1))
-             currencyTitle.append(currencyKey)
+        currencyTitle.append(currencyKey)
     }
     currencyTitle = currencyTitle.removingDuplicates()
-
+    
     return currencyTitle
 }
-
 extension Array where Element: Hashable {
     func removingDuplicates() -> [Element] {
         var addedDict = [Element: Bool]()
-
+        
         return filter {
             addedDict.updateValue(true, forKey: $0) == nil
         }
     }
-
+    
     mutating func removeDuplicates() {
         self = self.removingDuplicates()
     }
 }
 
+func putCurrencyIntoDictionary(){
+    let currencies = sortedCurrencies()
+    let dictionary = Dictionary(grouping: currencies, by: { String($0.unit.first!) })
+    currencyDictionary = dictionary
+}
 
